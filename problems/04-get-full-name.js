8d8f0b4e1c29613cf18f299847ea1102e036701a
Phat Giang <phatgiang1995@gmail.com>
2021-03-17 01:02:15 -0700

    Complete problem 4

--- problems/04-get-full-name.js
+++ problems/04-get-full-name.js
@@ -12,6 +12,7 @@ getFullName(p2); // => 'Charlie Brown'
 
 function getFullName(person) {
   // your code here
+  return person.firstName + " " + person.lastName;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
